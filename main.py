921704983084bed0da64f7f1c3208e255e046d85
akpax <austin.paxton00@gmail.com>
2024-01-02 15:02:35 -0800

    refactor main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from PyPDF2 import PdfReader
 from icecream import ic
 import re
 import pandas as pd
-import math
 from sklearn.cluster import DBSCAN
 import numpy as np
 import openpyxl
@@ -17,16 +16,13 @@ def read_full_pdf(file_path):
         reader = PdfReader(file)
         # Initialize an empty string to hold all text
         full_text = ""
-
         # Iterate over all the pages
         for page_num in range(0,len(reader.pages)):
             # Extract text from the page
             page = reader.pages[page_num]
             text = page.extract_text()
-
             # Add the text to the full text
             full_text += text
-
         return full_text
 
 def extract_lateral_load_cases(text):
@@ -48,48 +44,16 @@ def extract_entries_from_section(text):
     return lines
 
 def extract_entries_from_input(text):
-    #pattern = r"(\d+) (D\d+) (SW\d+) (\d+) (-?\d+\.?\d*\s+[a-zA-Z]+,\s*-?\d+\.?\d*\s+[a-zA-Z]+) (-?\d+\.?\d*\s+[a-zA-Z]+,\s*-?\d+\s*\d*\.?\d*\s+[a-zA-Z]+) (T[\w_]*\d+) (T[\w_]*\d+) "
-    #pattern = r"(\d+)\s+([a-zA-Z]+\d+-?[a-zA-Z]*)\s+(SW\d+)\s+(\d+)\s+(-?\d+\.?\d*\s+[a-zA-Z]+,-?\s*(?:-?\d+\.?\d*)?\s*[a-zA-Z]*\s*)\s*(-?\d+\.?\d*\s+[a-zA-Z]+,-?\s*(?:-?\d+\.?\d*)?\s*[a-zA-Z]*\s*)\s+(T[\w_]*\d+)\s+(T[\w_]*\d+)"
     pattern = "(\d+)\s+([a-zA-Z]+\d+-?[a-zA-Z]*)\s+(SW\d+)\s+(\d+)\s+\[([-\d.]+),\s+([-\d.]+)\]\s+\[([-\d.]+),\s+([-\d.]+)\]\s+([A-Za-z]\d+[A-Za-z]?)_([A-Za-z]\d+)\s+([A-Za-z]\d+[A-Za-z]?)_([A-Za-z]\d+)"
-
     lines = re.findall(pattern, text) # finds all cpatures groups and creates list of tuples
     return lines
 
 def check_for_entries(text: str) -> bool:
+    """
+    used in find_sections fucntion to remove sections without entries
+    """
     pattern = r"(\d+) (SW\d+) ([a-zA-Z]+\d+-?[a-zA-Z]*) (\d+\*?) (T[\w_]*\d+) (\d{1,3}(?:,\d{3})* [a-zA-Z]+) (T[\w_]*\d+) (\d{1,3}(?:,\d{3})* [a-zA-Z]+)"
     return bool(re.search(pattern,text))
-
-
-# def label_close_points(df, handle_column, coord_col, out_label="label",tolerance=3):
-#     # use 3" for any model per Tamara
-#     # Sort the DataFrame based on x-coordinates
-#     sorted_df = df.sort_values(by=coord_col)
-#     # Initialize label and a list to store the labels
-#     label = 0
-#     labels = [None] * len(sorted_df)
-#     # Iterate through the DataFrame rows
-#     for i in range(len(sorted_df)):
-#         if labels[i] is not None:
-#             continue
-
-#         labels[i] = label
-#         current_x = sorted_df.iloc[i][coord_col]
-
-#         # Check the next points in the sorted DataFrame
-#         for j in range(i+1, len(sorted_df)):
-#             if math.isclose(current_x, sorted_df.iloc[j][coord_col], abs_tol=tolerance):
-#                 if labels[j] is None:
-#                     labels[j] = label
-#                     print(f"l: {j} || Current x: {current_x} || Next x: {sorted_df.iloc[j][coord_col]}")
-#             else:
-#                 print(f"Breaking at {j}: Current x = {current_x}, Next x = {sorted_df.iloc[j][coord_col]}, Label = {label}")
-#                 print("__________________________________________________________________")
-#                 break
-#         label += 1
-
-#     # Map the labels back to the original DataFrame order
-#     sorted_df[out_label] = labels
-#     return df.merge(sorted_df[[out_label]], left_index=True, right_index=True, how='left')
 
 def convert_coord_pair_to_float(coord):
     if not type(coord) == str:
@@ -129,9 +93,9 @@ sections = find_sections(output_text)
 print(extract_entries_from_section("2638 SW122 L5-E 22 T7_x6 0 lbs T7_x6 0 lbs "))
 output_dict = {}
 # create output dict in folowing format {laod_case: [entry as tuple]}
-print(f"Length Sections: {len(sections)}")
-print(f"Length load cases: {len(load_cases)}")
+
 assert len(sections) == len(load_cases)
+# extract entries from section and pair entries with assocaited laod case in output_dict
 for i,section in enumerate(sections):
     lines = extract_entries_from_section(section)
     if not load_cases[i] in output_dict.keys():
@@ -153,20 +117,17 @@ output_cols = [
 
 output_dfs = []
 
+# add load case to each tuple entry from extraction and create dataframe for each load case
 for load_case, entries in output_dict.items():
     entries_w_load_case = [entry + (load_case,) for entry in entries ]
     df = pd.DataFrame(entries_w_load_case, columns=output_cols)
     output_dfs.append(df)
 
 
-# key output_dfs are same length befoe combining
-for df in output_dfs:
-    print(len(df))
-
 def ensure_df_in_list_same_length(df_list: list):
     assert all([len(df)==len(df_list[0]) for df in df_list])
 
-ensure_df_in_list_same_length(output_dfs)
+ensure_df_in_list_same_length(output_dfs)  #after creating datframes, each load case df should have same length
 
 
 output_df = pd.concat(output_dfs, ignore_index=True)
@@ -201,14 +162,9 @@ input_df.columns = input_cols
 
 input_raw_df = input_df # save initial input_df for logging purposes
 
-
-
-
 ############## create location keys for input df to determine stacked shear walls
 # remove repetiive columns and only keep necessary info for input_df for merge
 input_df = input_df[["handle","left_location", "right_location", "diaphragm"]]
-
-
 
 input_df[["x_left","y_left"]] = input_df.apply(lambda row: convert_coord_pair_to_float(row["left_location"]), axis=1, result_type="expand")
 input_df[["x_right","y_right"]] = input_df.apply(lambda row: convert_coord_pair_to_float(row["right_location"]), axis=1, result_type="expand")
@@ -223,8 +179,6 @@ grouped_sw_df = input_df.groupby("location_key", as_index=False)
 bad_walls = 0
 #check that allsw w same location tag are four levels
 for shearwall,frame in grouped_sw_df:
-    # print(f"Shearwall: {shearwall}")
-    # print(frame)
     if not len(frame) == 4:
         bad_walls+=1
         print("_____________________________________________________________________________")
@@ -267,9 +221,7 @@ delta_df = grouped_df.apply(find_delta_forces, force_cols=["left_tension","right
 delta_df = delta_df.reset_index(drop=True) # convert multi-index back to cols
 # result_df["max_delta"] = result_df[["delta_left_tension","delta_right_tension"]].max(axis=1) #find max delta between right and left holddowns
 
-
 # # Group by 'handle' and calculate the max force for each group
-# max_delta_force_id = result_df.groupby('handle')['max_delta'].idxmax()
 delta_grouped_df = delta_df.groupby("handle")
 
 delta_left_max_index = delta_grouped_df["delta_left_tension"].idxmax()
@@ -306,24 +258,8 @@ HD_df = pd.concat([LHD_df,RHD_df],axis=0, ignore_index=True)
 HD_max_df = HD_df.groupby("HD_type", as_index=False).max("tension").sort_values("tension",ascending=False)
 
 
-# Retain only the rows where the force equals the maximum force in its group
-# final_output_df = result_df.loc[max_delta_force_id]
-# print(final_output_df["load_case"])
-# print(final_output_df)
 
-
-################################################ KEEP CODE BELLOW 
-# # print(final_output_df)
-# final_output_df = final_output_df.groupby("location_key").apply(lambda frame: frame.sort_values("level")) #group by location key to see stacked walls
-
-# final_output_df = final_output_df[["handle","name","load_case","level","left_location","right_location","left_tension", 'delta_left_tension', "right_tension", 'delta_right_tension','max_delta']]
-
-# # print(len(final_output_df))
-# # print(len(input_df))
-# final_output_df.to_csv("final_output.csv", index=False) #set index to true bc handle was used in groupby and is now index
-
-
-# ##################### Initialize ExcelWriter for output #######################
+# ##################### Output Important dfs to different sheets in output.xlsx #######################
 with pd.ExcelWriter(output_path) as writer:
     HD_max_df.to_excel(writer, sheet_name="Holdown Summary", index=False, startrow=15) #leave room for MAR logo
     walls_max_delta_df.to_excel(writer, sheet_name='Wall Info', index=False, startrow=15)
@@ -335,7 +271,6 @@ with pd.ExcelWriter(output_path) as writer:
 wb = openpyxl.load_workbook(output_path)
 ws1 = wb["Holdown Summary"]
 ws2 = wb["Wall Info"]
-
 ws1.add_image(Image(logo_path), "A1")
 ws2.add_image(Image(logo_path), "A1")
 wb.save(output_path)
