50a475744a63ac65f833ad734b75f722e7f71ee0
akpax <austin.paxton00@gmail.com>
2024-01-03 19:31:21 -0800

    add comments and refactored main

--- GUI.py
+++ GUI.py
@@ -1,10 +1,3 @@
-# Import the required Libraries
-from tkinter import *
-from tkinter import ttk, filedialog
-from tkinter.filedialog import askopenfile
-import os
-
-
 from tkinter import Tk, Label, Button, filedialog
 
 class FileSelectorGUI:
@@ -20,7 +13,7 @@ class FileSelectorGUI:
         self.input_path_label = Label(self.win, text="")
         self.input_path_label.grid(row=0, column=1)
 
-        Label(self.win, text="Select a LSW output file (.pdf):", font=('Arial 13 bold')).grid(row=1, column=0,padx=30)
+        Label(self.win, text="Select a LSW tension force output file (.pdf):", font=('Arial 13 bold')).grid(row=1, column=0,padx=30)
         Button(self.win, text="Browse", command=self.select_output_file).grid(row=1, column=2,ipadx=10,ipady=3, padx=30)
         self.output_path_label = Label(self.win, text="")
         self.output_path_label.grid(row=1, column=1)
@@ -44,49 +37,14 @@ class FileSelectorGUI:
         self.win.mainloop()
         return self.input_file_path, self.output_file_path
 
-def create_file_selector():
+def create_file_selector_window():
     gui = FileSelectorGUI()
     return gui.run()
 
 
-# # Set the geometry of tkinter frame
-# # win.geometry("700x350")
 
-# def get_input_file_path(win):
-#    global LSW_input_file_path
-#    file = filedialog.askopenfile(mode='r', filetypes=[('Text Files', '*.txt')])
-#    LSW_input_file_path = os.path.abspath(file.name)
-#    Label(win, text= f'"{LSW_input_file_path}"', font=('Arial 13 italic')).grid(row=0,column=1, ipadx=50, ipady=10)
-#    print(LSW_input_file_path)
-#    return LSW_input_file_path
 
-# def get_output_file_path(win):
-#    global  LSW_output_file_path
-#    file = filedialog.askopenfile(mode='r', filetypes=[('PDF Files', '*.pdf')])
-#    LSW_output_file_path = os.path.abspath(file.name)
-#    Label(win, text= f'"{LSW_output_file_path}"', font=('Arial 13 italic')).grid(row=1,column=1, ipadx=50, ipady=10)
-#    print(LSW_output_file_path)
-#    return LSW_output_file_path
-      
-
-# def close_window(win):
-#    win.destroy()
-
-# # Add a Label widget
-# def create_input_files_window():
-#     # Create an instance of tkinter frame
-#     win = Tk()
-#     win.title("LSW Holdown Report Generator")
-#     Label(win, text="Please select LSW .txt input file:", font=('Arial 13 bold')).grid(row=0,column=0, ipadx=50, ipady=10)
-#     ttk.Button(win, text="Browse", command= lambda: get_input_file_path(win)).grid(row=0, column=2)
-
-#     Label(win, text="Please select LSW .pdf output file:", font=('Arial 13 bold')).grid(row=1,column=0, ipadx=50, ipady=10)
-#     ttk.Button(win, text="Browse", command=lambda: get_output_file_path(win)).grid(row=1, column=2)
-
-#     ttk.Button(win,text="Submit", command=lambda: close_window(win)).grid(row=2, column=0)
-#     return win
-
-def create_output_alert(output_file_folder):
+def create_output_alert_window(output_file_folder):
    win = Tk()
    win.title("LSW Holdown Report Generator - Success")
    Label(win, text='Analysis Successfull!' ,font=('Arial 13 bold')).grid(row=0,padx=50,pady=10)
@@ -94,7 +52,3 @@ def create_output_alert(output_file_folder):
    Button(win,text="Exit", command=win.destroy).grid(row=2)
    win.mainloop()
    return win
-
-
-if __name__ == "__main__":
-   create_file_selector()
--- main.py
+++ main.py
@@ -6,17 +6,21 @@ from sklearn.cluster import DBSCAN
 import numpy as np
 import openpyxl
 from openpyxl.drawing.image import Image
-from GUI import create_file_selector, create_output_alert
+from GUI import create_file_selector_window, create_output_alert_window
 from datetime import datetime
 import os
 import sys
 
 
 
-
-def read_full_pdf(file_path):
+################### functions ############################
+##### fucntions for reading pdf #####
+def read_full_pdf(pdf_file_path: str):
+    """
+    Reads all pages of a pdf and returns contents as a string
+    """
     # Open the PDF file
-    with open(file_path, 'rb') as file:
+    with open(pdf_file_path, 'rb') as file:
         reader = PdfReader(file)
         # Initialize an empty string to hold all text
         full_text = ""
@@ -29,37 +33,52 @@ def read_full_pdf(file_path):
             full_text += text
         return full_text
 
-def extract_lateral_load_cases(text):
-    # Regular expression to find all lateral load cases
+def extract_lateral_load_cases(text: str):
+    """
+    Uses regex to find lateral load cases in a string and returns them as a list
+    """
     pattern = r"Lateral Load = (.*?)'"
     lateral_load_cases = re.findall(pattern, text)
     return [load_case.replace(" ","") for load_case in lateral_load_cases]
 
-def find_sections(text):
+def find_sections(text: str):
+    """
+    Uses break text to divide long string into sections and returns sections as list
+    Sections are checked to see if they contain entries which is row in our pdf table
+    """
     # Splitting the text at each occurrence of the break text
     break_text = "Handle"
     sections = re.split(break_text, text)
     sections_w_entries = [section for section in sections if check_for_entries(section)]
     return sections_w_entries
 
-def extract_entries_from_section(text):
+def extract_entries_from_section(text: str):
+    """
+    Uses regex to find entries (row of data in pdf text) in text and returns each row as list of tuples
+    """
     pattern = r"(\d+) (SW\d+) ([a-zA-Z]+\d+-?[a-zA-Z]*) (\d+\*?) (T[\w_]*\d+) (\d{1,3}(?:,\d{3})* [a-zA-Z]+) (T[\w_]*\d+) (\d{1,3}(?:,\d{3})* [a-zA-Z]+)"
     lines = re.findall(pattern, text) # finds all cpatures groups and creates list of tuples
     return lines
 
-def extract_entries_from_input(text):
-    pattern = "(\d+)\s+([a-zA-Z]+\d+-?[a-zA-Z]*)\s+(SW\d+)\s+(\d+)\s+\[([-\d.]+),\s+([-\d.]+)\]\s+\[([-\d.]+),\s+([-\d.]+)\]\s+([A-Za-z]\d+[A-Za-z]?)_([A-Za-z]\d+)\s+([A-Za-z]\d+[A-Za-z]?)_([A-Za-z]\d+)"
-    lines = re.findall(pattern, text) # finds all cpatures groups and creates list of tuples
-    return lines
 
 def check_for_entries(text: str) -> bool:
     """
-    used in find_sections fucntion to remove sections without entries
+    Uses regex to check for entry (row of data) and returns boolean
     """
     pattern = r"(\d+) (SW\d+) ([a-zA-Z]+\d+-?[a-zA-Z]*) (\d+\*?) (T[\w_]*\d+) (\d{1,3}(?:,\d{3})* [a-zA-Z]+) (T[\w_]*\d+) (\d{1,3}(?:,\d{3})* [a-zA-Z]+)"
     return bool(re.search(pattern,text))
 
+def ensure_df_in_list_same_length(df_list: list):
+    """
+    Checks that each dataframe in list is the same length
+    """
+    assert all([len(df)==len(df_list[0]) for df in df_list])
+
+
+##### functions for manipulating dataframes #####
 def convert_coord_pair_to_float(coord):
+    """
+    Takes string in following format: [1234.2, -134] and returns each number as a float 1234.1, -134"""
     if not type(coord) == str:
         coord = coord.str
     coord = coord.strip()
@@ -69,19 +88,68 @@ def convert_coord_pair_to_float(coord):
     y = float(y)
     return x,y
 
-def label_close_points_with_dbscan(df, handle_column, coord_cols: list, out_label="label", eps=3):
+def remove_comma_convert_to_float(x):
+    """
+    Takes a string with expected format "12,3423 lb" and converts number to float
+    """
+    number,unit = x.split(" ")  # TO DO create fucntion that takes unti and appends to column title
+    number = number.replace(",","")
+    number = float(number)
+    return number
+
+def label_close_points_with_dbscan(df, coord_cols: list, out_label="label", eps=3):
+    """
+    Use sklearn DBSCAN alogrithm to create a label column from x,y coordiante colummns
+    
+    Inputs:
+    df (pandas.DataFrame): dataframe contianing coord columns
+    coord_cols (list): name of columns containing coordinates
+    out_label (str): name for new label column
+    eps (int): DBSCAN algorithm paramter; maximum distance between two samples for one to be considered
+          as in the neighborhood of the other. 
+    """
     X = df[coord_cols].values
     clustering = DBSCAN(eps=eps, min_samples=2).fit(X)
     df[out_label] = clustering.labels_
     return df
 
 def create_location_key(label_l, label_r):
+    """
+    Applied to a datframe via lambda fucntion; 
+    Combines two strings with an "_"
+    """
     return f"{str(label_l)}_{str(label_r)}"
 
+def extract_level(diaphragm_label):
+    """
+    Extract digits from diaphragm string and returns them as integer
+    """
+    assert isinstance(diaphragm_label,str)
+    digit_list = [character for character in diaphragm_label if character.isdigit()]
+    level = "".join([digit for digit in digit_list])
+    return int(level)
+
+def find_delta_forces(frame, force_cols: list):
+    """
+    This function is intended to be applied to each dataframe that is grouped by load case and shear wall.
+    It returns the elta force between the wall at current level and the wall above it.
+    The top wall returns Nan (no wall above it) and it is given its on force value as the delta force
+    """
+    # Sort the group if needed
+    frame = frame.sort_values("level")
+    # Calculate the difference between consecutive rows for 'left_tension_float'
+    for force_col in force_cols:
+        frame[f"delta_{force_col}"] = frame[force_col] - frame[force_col].shift(-1)
+        frame[f"delta_{force_col}"] = frame[f"delta_{force_col}"].fillna(frame[force_col])
+    return frame
 
 
-##### Output Related Functions
+
+##### Output Related Functions #####
 def append_date_and_time(prefix):
+    """
+    Appends current date to the prefix string
+    """
     date = datetime.now()
     formated_date = date.strftime("%m-%d-%Y")
     return f"{prefix}_{formated_date}"
@@ -99,30 +167,30 @@ def create_output_folder_path(name):
     return output_folder_path
 
 def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
+    """ 
+    Get absolute path to resource, works for dev and for PyInstaller 
+    Note: sys._MEIPASS is a temporary folder for PyInstaller, necessary to load images folder into bundled app
+    """
     base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
 
 ####################### Key variables #####################################
-# LSW_output_file_path = r"test_files/Thornton SW Output.pdf"
-# LSW_input_file_path = r"test_files/Thornton Input.txt"
 eps = 2
 logo_path = resource_path(r"images/MSD Full Logo.jpg")
 
-LSW_input_file_path, LSW_output_file_path = create_file_selector()
+LSW_input_file_path, LSW_output_file_path = create_file_selector_window()
 
 
 # # ############### Read output pdf and store as DF #################
 output_text = read_full_pdf(LSW_output_file_path)
 
-### read output file
+##### parse output file to output dict in folowing format {load_case: [entry: tuple]} #####
 load_cases = extract_lateral_load_cases(output_text)
 sections = find_sections(output_text)
 output_dict = {}
-# create output dict in folowing format {load_case: [entry: tuple]}
 
-assert len(sections) == len(load_cases)
+assert len(sections) == len(load_cases) # if assertion fails, there was an error readig the output_pdf
 # extract entries from section and pair entries with assocaited laod case in output_dict
 for i,section in enumerate(sections):
     lines = extract_entries_from_section(section)
@@ -152,26 +220,12 @@ for load_case, entries in output_dict.items():
     output_dfs.append(df)
 
 
-def ensure_df_in_list_same_length(df_list: list):
-    assert all([len(df)==len(df_list[0]) for df in df_list])
-
 ensure_df_in_list_same_length(output_dfs)  #after creating datframes, each load case df should have same length
-
-
 output_df = pd.concat(output_dfs, ignore_index=True)
-
 output_raw_df = output_df #save raw output for report later
-
-def remove_comma_convert_to_float(x):
-    number,unit = x.split(" ")  # TO DO create fucntion that takes unti and appends to column title
-    number = number.replace(",","")
-    number = float(number)
-    return number
 
 output_df["left_tension"] = output_df.apply(lambda row: remove_comma_convert_to_float(row["left_tension"]), axis=1)
 output_df["right_tension"] = output_df.apply(lambda row: remove_comma_convert_to_float(row["right_tension"]), axis=1)
-
-
 
 ############### Read Input Text and Store as Df ###################
 input_cols = [
@@ -184,44 +238,40 @@ input_cols = [
     "LHD",
     "RHD"
 ]
-input_df = pd.read_table(LSW_input_file_path, header=None)
-input_df = input_df.iloc[:,:-1]
-input_df.columns = input_cols
 
+input_df = pd.read_table(LSW_input_file_path, header=None)
+input_df = input_df.iloc[:,:-1] # drops last column which is filled with Nan values
+input_df.columns = input_cols
 input_raw_df = input_df # save initial input_df for logging purposes
 
 ############## create location keys for input df to determine stacked shear walls
-# remove repetiive columns and only keep necessary info for input_df for merge
+# remove only keep necessary columns for input_df for merge
 input_df = input_df[["handle","left_location", "right_location", "diaphragm"]]
 
 input_df[["x_left","y_left"]] = input_df.apply(lambda row: convert_coord_pair_to_float(row["left_location"]), axis=1, result_type="expand")
 input_df[["x_right","y_right"]] = input_df.apply(lambda row: convert_coord_pair_to_float(row["right_location"]), axis=1, result_type="expand")
 
-input_df = label_close_points_with_dbscan(input_df, "handle", ['x_left','y_left'], out_label="label_l",eps=2)
-input_df = label_close_points_with_dbscan(input_df, "handle", ['x_right','y_right'], out_label="label_r",eps=2)
+print(input_df)
+input_df = label_close_points_with_dbscan(input_df, ['x_left','y_left'], out_label="label_l",eps=2) #create unqiue lable for similar left coord
+input_df = label_close_points_with_dbscan(input_df, ['x_right','y_right'], out_label="label_r",eps=2) #create unqiue lable for similar right coord
 
 input_df["location_key"] = input_df.apply(lambda row: create_location_key(row["label_l"], row["label_r"]), axis=1)
 
 grouped_sw_df = input_df.groupby("location_key", as_index=False)
 
-bad_walls = 0
-#check that allsw w same location tag are four levels
-for shearwall,frame in grouped_sw_df:
-    if not len(frame) == 4:
-        bad_walls+=1
-        # print("_____________________________________________________________________________")
-        # print(f"Shearwall: {shearwall}")
-        # # print(frame.head())
-        # print(frame[['handle','x_left','xl_label','y_left','yl_label','x_right','xr_label','y_right','yr_label']])
-        # print("_____________________________________________________________________________")
+# bad_walls = 0
+# #check that allsw w same location tag are four levels
+# for shearwall,frame in grouped_sw_df:
+#     if not len(frame) == 4:
+#         bad_walls+=1
+#         # print("_____________________________________________________________________________")
+#         # print(f"Shearwall: {shearwall}")
+#         # # print(frame.head())
+#         # print(frame[['handle','x_left','xl_label','y_left','yl_label','x_right','xr_label','y_right','yr_label']])
+#         # print("_____________________________________________________________________________")
     
 # print(f"bad walls{ bad_walls}")
 
-def extract_level(diaphragm_label):
-    assert isinstance(diaphragm_label,str)
-    digit_list = [character for character in diaphragm_label if character.isdigit()]
-    level = "".join([digit for digit in digit_list])
-    return int(level)
 
 input_df["level"] = input_df.apply(lambda row: extract_level(row["diaphragm"]), axis=1)
 
@@ -235,60 +285,46 @@ df = pd.merge(output_df, input_df, on="handle", how="left")
 ########################### group by load case and shear wall and find delta forces for each side
 grouped_df = df.groupby(["load_case","location_key"])#.sort_values("level")
 
-def find_delta_forces(frame, force_cols: list):
-    # Sort the group if needed
-    frame = frame.sort_values("level")
-    # Calculate the difference between consecutive rows for 'left_tension_float'
-    for force_col in force_cols:
-        frame[f"delta_{force_col}"] = frame[force_col] - frame[force_col].shift(-1)
-        frame[f"delta_{force_col}"] = frame[f"delta_{force_col}"].fillna(frame[force_col])
-    return frame
-
 delta_df = grouped_df.apply(find_delta_forces, force_cols=["left_tension","right_tension"])
 
 delta_df = delta_df.reset_index(drop=True) # convert multi-index back to cols
-# result_df["max_delta"] = result_df[["delta_left_tension","delta_right_tension"]].max(axis=1) #find max delta between right and left holddowns
 
-# # Group by 'handle' and calculate the max force for each group
+##### Group by 'handle' and calculate the max delta force for each holdown in shearwall #####
 delta_grouped_df = delta_df.groupby("handle")
 
-delta_left_max_index = delta_grouped_df["delta_left_tension"].idxmax()
+#obtain index of maximum values for left and right HD
+delta_left_max_index = delta_grouped_df["delta_left_tension"].idxmax() 
 delta_right_max_index = delta_grouped_df["delta_right_tension"].idxmax()
 
+# filter for max delta vlaue based on index and pull releveant columns for output report
 walls_max_delta_left_df = delta_df.loc[delta_left_max_index][["handle","name","delta_left_tension","LHD","load_case","left_location"]]
 walls_max_delta_left_df.rename(columns={"load_case": "load_case_left"},inplace=True)
-
 walls_max_delta_right_df = delta_df.loc[delta_right_max_index][["handle","delta_right_tension","RHD","load_case","right_location", "location_key", "level"]]
 walls_max_delta_right_df.rename(columns={"load_case": "load_case_right"},inplace=True)
 
-walls_max_delta_df = walls_max_delta_left_df.merge(walls_max_delta_right_df,how="outer", on="handle")
+assert(len(walls_max_delta_left_df)==len(walls_max_delta_right_df)) #dfs should be same length
+walls_max_delta_df = walls_max_delta_left_df.merge(walls_max_delta_right_df,how="outer", on="handle") 
 
-print(walls_max_delta_df)
-print(len(walls_max_delta_left_df))
-print(len(walls_max_delta_right_df))
-print(len(walls_max_delta_df))
 
-# groupy by location key and sort by level
+# groupy by location key and sort by level for output report clarity
 walls_max_delta_df = walls_max_delta_df.groupby("location_key",as_index=False).apply(lambda frame: frame.sort_values("level"))
 walls_max_delta_df.drop(columns="location_key",inplace=True)
 
 
 ############################################## create tiedown schedule
-# take holdowns and delta forces for left and right and concatenated along rows
+# holdowns are not always same on left and right side for each hshearwall
+# stack the holddown type and tension force for left and rght shearwalls
 LHD_df = walls_max_delta_df[["LHD","delta_left_tension"]]
 LHD_df.rename(columns={"LHD": "HD_type", "delta_left_tension": "tension"},inplace=True)
-
 RHD_df = walls_max_delta_df[["RHD","delta_right_tension"]]
 RHD_df.rename(columns={"RHD": "HD_type", "delta_right_tension": "tension"}, inplace=True)
+HD_df = pd.concat([LHD_df,RHD_df],axis=0, ignore_index=True) # concatenat along rows
 
-HD_df = pd.concat([LHD_df,RHD_df],axis=0, ignore_index=True)
-
-HD_max_df = HD_df.groupby("HD_type", as_index=False).max("tension").sort_values("tension",ascending=False)
-
+HD_max_df = HD_df.groupby("HD_type", as_index=False).max("tension").sort_values("tension",ascending=False) 
 
 
 # ##################### Output Important dfs to different sheets in output.xlsx #######################
-### create output folder on desktop
+# create output folder path in CWD
 output_folder_path = create_output_folder_path("LSW_HoldownReport")
 print(f"- Output Folder: {output_folder_path}")
 #create folder for output
@@ -298,10 +334,9 @@ if not os.path.exists(output_folder_path):
 
 xlsx_output_path = os.path.join(output_folder_path, "output.xlsx")
 
-
 with pd.ExcelWriter(xlsx_output_path) as writer:
     HD_max_df.to_excel(writer, sheet_name="Holdown Summary", index=False, startrow=15) #leave room for MAR logo
-    walls_max_delta_df.to_excel(writer, sheet_name='Wall Info', index=False, startrow=15)
+    walls_max_delta_df.to_excel(writer, sheet_name='Wall Info', index=False, startrow=15) #leave room for MAR logo
     output_raw_df.to_excel(writer, sheet_name='LSW output data', index=False)
     input_raw_df.to_excel(writer, sheet_name='LSW input data', index=False)
     
@@ -315,7 +350,7 @@ ws2.add_image(Image(logo_path), "A1")
 wb.save(xlsx_output_path)
 
 #### display success message
-create_output_alert(output_folder_path)
+create_output_alert_window(output_folder_path)
 
 
 
